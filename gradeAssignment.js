3af59441fa31f176beda5b407ff742abb682c421
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-10-07 18:55:46 +0800

    If Else If Statement Activity

--- gradeAssignment.js
+++ gradeAssignment.js
@@ -2,11 +2,11 @@ let score = prompt("Enter your score:");
 score = parseInt(score);
 
 if (score >= 90){
-    console.log("Your grade is: Excellent");
+    console.log("Your grade is: Excellent.");
 }else if (score >= 80 && score <= 89){
-    console.log("Your grade is: Good");
+    console.log("Your grade is: Good.");
 } else if (score >= 70 && score <= 79){
-    console.log("Your grade is: Fair");
+    console.log("Your grade is: Fair.");
 }else{
-    console.log("Your grade is: Needs Improvement");
+    console.log("Your grade is: Needs Improvement.");
 }
